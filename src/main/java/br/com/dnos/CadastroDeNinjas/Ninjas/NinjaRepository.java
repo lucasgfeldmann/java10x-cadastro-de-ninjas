916cee7de07d2e164bd9a9a0bb3139b75e9d72b3
LucasGFeldmann <lucasgfeldmann@gmail.com>
2025-02-24 14:50:37 -0300

    feat: create NinjaRepository and MissoesRepository

---
+++ src/main/java/br/com/dnos/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -0,0 +1,6 @@
+package br.com.dnos.CadastroDeNinjas.Ninjas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
+}
