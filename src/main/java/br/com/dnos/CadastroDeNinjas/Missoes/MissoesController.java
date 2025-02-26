2b3ce29c315298c346952d967298b7fdd20a51e4
LucasGFeldmann <lucasgfeldmann@gmail.com>
2025-02-26 14:31:55 -0300

    feat: Cria as rotas do MissoesController com mock

---
+++ src/main/java/br/com/dnos/CadastroDeNinjas/Missoes/MissoesController.java
@@ -0,0 +1,31 @@
+package br.com.dnos.CadastroDeNinjas.Missoes;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
+public class MissoesController {
+
+    @GetMapping("/listar")
+    public String listarMissao() {
+        return "Missões listadas com sucesso";
+    }
+
+
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Missão criada com sucesso";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "Missão alterada com sucesso";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "Missão deletar com sucesso";
+    }
+
+}
